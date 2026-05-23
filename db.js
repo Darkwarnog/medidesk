05f841eb1a0a6e8d93266bc09ba2f4b076de79fc
Alejandro <alejandrosalas629@gmail.com>
2026-05-23 02:44:05 -0600

    debug bcrypt

--- db.js
+++ db.js
@@ -2,19 +2,19 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const conexion = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT || 3306
+  host:     process.env.DB_HOST,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port:     process.env.DB_PORT || 3306
 });
 
 conexion.connect((err) => {
-    if (err) {
-        console.log('Error conectando MySQL:', err);
-    } else {
-        console.log('Conectado a MySQL');
-    }
+  if (err) {
+    console.error('Error conectando MySQL:', err.message);
+  } else {
+    console.log('Conectado a MySQL correctamente');
+  }
 });
 
 module.exports = conexion;
